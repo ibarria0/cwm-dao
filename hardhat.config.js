6fc7c0b49969883a675c9894ac30babc477722fc
Jose Varela <joservarelaf@gmail.com>
2022-02-01 19:58:53 -0500

    Add typechain

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,8 @@
  */
 
 require('dotenv').config();
+require('@typechain/hardhat');
+require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-waffle");
 require("./tasks/faucet");
 require("./tasks/USDC_faucet");
@@ -28,4 +30,10 @@ module.exports = {
          accounts: [`0x${PRIVATE_KEY}`]
       }
    },
+   typechain: {
+    outDir: 'types',
+    target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
+  },
 };
