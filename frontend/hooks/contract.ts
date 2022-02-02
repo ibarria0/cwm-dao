77b4c0e014e45f8eeaa1499a00a4338809533fb4
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2022-02-01 20:32:55 -0500

    Interface working

--- frontend/hooks/contract.ts
+++ frontend/hooks/contract.ts
@@ -15,7 +15,7 @@ declare global {
   }
 }
 
-const ADDRESS_CWM_BROKER = "0x";
+const ADDRESS_CWM_BROKER = "0xFAKE_ADDRESS";
 const ADDRESS_TOKEN = "0x2791bca1f2de4661ed88a30c99a7a9449aa84174";
 
 export function useContract() {
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  TextField,
+} from "@mui/material";
 import { BigNumber, ethers } from "ethers";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 import { useContract } from "../hooks/contract";
 
 const Home: NextPage = () => {
+  const [value, setValue] = useState(0);
   const [address, setAddress] = useState("");
   const [balance, setBalance] = useState(BigNumber.from(0));
   const [erc20Balance, setERC20Balance] = useState(BigNumber.from(0));
@@ -22,6 +31,34 @@ const Home: NextPage = () => {
     erc20Contract.balanceOf(address).then(setERC20Balance);
   }, [erc20Contract, address]);
 
+  async function handleApprove() {
+    if (value < 1) {
+      alert(`Value needs to be above 0: ${value}`);
+      return;
+    }
+
+    const response = await erc20Contract?.approve(
+      address,
+      ethers.utils.parseEther(value.toString())
+    );
+    await response?.wait();
+
+    alert("Transfer Approved");
+  }
+
+  async function handleDeposit() {
+    if (value < 1) {
+      alert(`Value needs to be above 0: ${value}`);
+      return;
+    }
+
+    const response = await brokerContract?.deposit(
+      ethers.utils.parseEther(value.toString())
+    );
+    await response?.wait();
+    alert("Tokens deposited");
+  }
+
   return (
     <>
       <Head>
@@ -30,8 +67,36 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>{`Balance: ${ethers.utils.formatEther(balance)}`}</div>
-      <div>{`USDC: ${ethers.utils.formatEther(erc20Balance)}`}</div>
+      <Container>
+        <Card>
+          <CardContent className={styles.form}>
+            <TextField
+              label="Balance"
+              value={ethers.utils.formatEther(balance)}
+              disabled
+            />
+
+            <TextField
+              label="USCD"
+              value={ethers.utils.formatEther(erc20Balance)}
+              disabled
+            />
+
+            <TextField
+              id="outlined-number"
+              label="Amount to Deposit"
+              type="number"
+              value={value}
+              onChange={(event) => setValue(parseInt(event.target.value))}
+            />
+          </CardContent>
+
+          <CardActions>
+            <Button onClick={handleApprove}>Do Approve</Button>
+            <Button onClick={handleDeposit}>Do Deposit</Button>
+          </CardActions>
+        </Card>
+      </Container>
     </>
   );
 };
