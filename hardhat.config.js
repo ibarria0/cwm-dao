c0ffa889294484201c9733e7015c1ece218c6b54
Ivan Barria <ibarria0@gmail.com>
2022-02-13 17:29:52 -0500

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,28 +5,39 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require("@typechain/hardhat");
+require("@nomiclabs/hardhat-ethers")
+require("@nomiclabs/hardhat-etherscan");
 require("./tasks/faucet");
 require("./tasks/USDC_faucet");
 
-// const { POLYGON_MAIN_URL, MUMBAI_URL, PRIVATE_KEY } = process.env;
+const { POLYGON_MAIN_URL, MUMBAI_URL, PRIVATE_KEY, POLYSCAN_API_KEY } = process.env;
 
 module.exports = {
   solidity: {
-      compilers: [{version: '0.8.2'}],
+      compilers: [{version: '0.8.7'}],
+  },
+  etherscan: {
+      apiKey: {
+          polygon: POLYSCAN_API_KEY,
+          polygonMumbai: POLYSCAN_API_KEY 
+      }
   },
   networks: {
       hardhat: {
-      //     forking: {
-      //       url: POLYGON_MAIN_URL,
-      //       blockNumber: 24365626 
-      //     },
-      // },
-      // mumbai: {
-      //    url: MUMBAI_URL,
-      //    accounts: [`0x${PRIVATE_KEY}`]
+          forking:{
+              url: POLYGON_MAIN_URL
+          }
+      },
+      polygon: {
+         url: POLYGON_MAIN_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      },
+      mumbai: {
+         url: MUMBAI_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
-   },
-    typechain: {
-      outDir: 'frontend/typechain',
-    }
+  },
+  typechain: {
+    outDir: 'frontend/typechain',
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,7 @@
 async function main() {
+  let ADMIN_ROLE = ethers.utils.id("DEFAULT_ADMIN_ROLE")
   let minterRole = ethers.utils.id("MINTER_ROLE");
+  let snapshotRole = ethers.utils.id("SNAPSHOT_ROLE");
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
@@ -7,18 +9,41 @@ async function main() {
 
   const Token = await ethers.getContractFactory("CwmTokenV2");
   const token = await Token.deploy();
-
   console.log("Token address:", token.address);
+  await token.deployed();
+
+  const txHash = token.deployTransaction.hash;
+  console.log(`Tx hash: ${txHash}\nWaiting for transaction to be mined...`);
+  const txReceipt = await ethers.provider.waitForTransaction(txHash);
+
+  console.log("Contract address:", txReceipt.contractAddress);
+
+  tx = await token.grantRole(ADMIN_ROLE, deployer.address);
+  await tx.wait()
+
+  if (await token.hasRole(ADMIN_ROLE, deployer.address)) {
+      console.log("Grant roles to deployer");
+      console.log("TOKEN ADMIN address:", deployer.address);
+  }
 
   const Broker = await ethers.getContractFactory("CwmBroker");
-  const broker = await Broker.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", token.address, "0x5121911a192245e1d2cb78a4d527b52ef41eeb77", "0x3ba0648fc91550fdb274bad53aa1fd7924b79ff9");
-
+  const broker = await Broker.deploy("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", token.address, "0x5121911a192245e1d2cb78a4d527b52ef41eeb77", "0x3C618bA6a957D53998a182A0b4a27D8F240DB1bf");
+  await broker.deployed();
   console.log("Broker address:", broker.address);
 
-  token.grantRole(minterRole, broker.address);
-  if (token.hasRole(minterRole, broker.address)) {
+  tx = await token.grantRole(minterRole, broker.address);
+  await tx.wait()
+
+  if (await token.hasRole(minterRole, broker.address)) {
       console.log("Grant roles to broker");
       console.log("Minter address:", broker.address);
+  }
+
+  tx = await token.grantRole(snapshotRole, broker.address);
+  await tx.wait()
+  if (await token.hasRole(snapshotRole, broker.address)) {
+      console.log("Grant snapshot role");
+      console.log("Snapshot address:", "0x304BD332669C040a69c0734E74d43371A69dD559");
   }
 
 }
